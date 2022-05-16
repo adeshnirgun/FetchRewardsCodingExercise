1615d3b4ff0ba365e74af270706a71ead2ddd204
Adesh Nirgun <55123605+adeshnirgun@users.noreply.github.com>
2022-05-15 20:33:14 -0700

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -4,55 +4,57 @@ from flask import request
 app = Flask(__name__)
 from datetime import datetime as dt
 
-global_data = []
+#global_txn_data contains all transaction records
+global_txn_data = []
 
-
-#/dump route: Returns global data (used for testing purposes)
-@app.route('/dump')
-def Dump():
-    return jsonify(global_data)
-
-#/add route: 
+#/add: Adds a new transaction to global_txn_data, returns the payload
 @app.route('/add')
 def Add():
     r = request.args
     payload = r.to_dict()
     payload["points"] = int(payload["points"])
-    global_data.append(payload)
-    return jsonify(global_data)
+    global_txn_data.append(payload)
+    return payload
 
+#/spend: Deducts points from each payer in chronological order of transactions, returns a list of points deducted from payers  
 @app.route('/spend')
 def Spend():
     points = request.args.get('points')
     numPoints = int(points)
-    sorted_data = sorted(global_data, key = lambda i: i['timestamp'])
+    #Sorts global_txn_data by timestamp
+    sorted_data = sorted(global_txn_data, key = lambda i: i['timestamp'])
     spend_data = []
-    for pay in sorted_data:
+    #Iterates through sorted_data and deducts each txn's points from numPoints till numPoints <=0
+    #The deducted balance from each txn is assigned in balance
+    for txn in sorted_data:
         if numPoints <= 0:
             break
-        pointInt = int(pay["points"])
+        pointInt = int(txn["points"])
         numPoints -= pointInt
         if pointInt > numPoints:
             balance = (numPoints + pointInt) * -1
         else:
             balance = pointInt * -1  
-        entry = {"payer": pay["payer"], "points": balance}
-        listofbool = [True for elem in spend_data if pay["payer"] in elem.values()]
+        entry = {"payer": txn["payer"], "points": balance}
+        #returns a list of booleans where a duplicate payer is found
+        listofbool = [True for elem in spend_data if txn["payer"] in elem.values()]
+        #if there is a duplicate, update the deducted points instead of creating a new entry in spend_data
         if any(listofbool):
             for elem in spend_data:
                 if elem["payer"] == entry["payer"]:
                     elem["points"] += entry["points"]
         else:
             spend_data.append(entry)
+    #create a current timestamp for each deduction and append it to global_txn_data 
     time_dict = {'timestamp' :  dt.now().strftime("%Y-%m-%dT%H:%M:%SZ")}
-    [global_data.append(i|time_dict) for i in spend_data]
+    [global_txn_data.append(i|time_dict) for i in spend_data]
     return jsonify(spend_data) 
 
-
+#/balance: returns each payers' total number of points
 @app.route('/balance')
 def returnBalance():
     updatedBalance={}
-    for data in global_data:
+    for data in global_txn_data:
         if data["payer"] in updatedBalance:
             newBalance = updatedBalance[data["payer"]] + data["points"]
             updatedBalance[data["payer"]] = newBalance
